93ea359aab5adaf27d763c38b592139ee8205844
Trishtan Adhikari <adhikaritrishtan77@gmail.com>
2025-02-15 13:45:08 +0545

    initialize backend with Express, dotenv, and basic server setup

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+
+const express = require("express")
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+
+module.exports = app
